9dd19b6210225f3ec1b296b3a9f5e325d4f534f5
Grant Lemons <grant0lemons@gmail.com>
2024-02-10 08:56:02 -0700

    style main content of page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,11 @@
 	}
 </script>
 
-<div class="w-full h-full flex flex-col justify-center items-center">
-	<h1 class="text-2xl font-bold">StandUp</h1>
-	<h2 class="text-xl">Stay fit.</h2>
-	<Button on:click={openModal}>Take photo</Button>
+<div class="w-full h-full flex flex-col space-y-6 justify-center items-center">
+	<div class="flex flex-col justify-center items-center">
+		<h1 class="text-2xl font-bold">StandUp</h1>
+		<h2 class="text-xl">Stay fit.</h2>
+	</div>
+	<Button class="w-80" on:click={openModal}>Take Photo</Button>
 	<CameraModal bind:showModal />
 </div>
