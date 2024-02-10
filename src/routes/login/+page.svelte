495730a553dfe02c9e35fd829521ec98c335d690
Grant Lemons <grant0lemons@gmail.com>
2024-02-10 07:47:58 -0700

    fix eof error, format, and add deploy script

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-  import { getAuth, GoogleAuthProvider, EmailAuthProvider } from 'firebase/auth';
-  import * as firebaseui from 'firebaseui'
-  import 'firebaseui/dist/firebaseui.css'
+	import { getAuth, GoogleAuthProvider, EmailAuthProvider } from 'firebase/auth';
+	import * as firebaseui from 'firebaseui';
+	import 'firebaseui/dist/firebaseui.css';
 	import { onDestroy, onMount } from 'svelte';
 
-  let ui: firebaseui.auth.AuthUI | undefined;
-  
-  onMount(async () => {
-    let auth = getAuth();
+	let ui: firebaseui.auth.AuthUI | undefined;
+
+	onMount(async () => {
+		let auth = getAuth();
+	});
 
 	onMount(async () => {
 		let auth = getAuth();
@@ -19,21 +20,18 @@
 		if (auth.currentUser != null) {
 			await goto('/user');
 		}
-    var uiConfig = {
-      signInSuccessUrl: '/user',
-      signInOptions: [
-        GoogleAuthProvider.PROVIDER_ID,
-      ],
-    };
+		var uiConfig = {
+			signInSuccessUrl: '/user',
+			signInOptions: [GoogleAuthProvider.PROVIDER_ID]
+		};
 
 		ui = new firebaseui.auth.AuthUI(auth);
 		ui.start('#firebaseui-auth-container', uiConfig);
 	});
 
-  onDestroy(() => {
-    if (ui) ui.delete();
-  })
-
+	onDestroy(() => {
+		if (ui) ui.delete();
+	});
 </script>
 
 <div class="w-full h-full flex flex-col justify-center items-center font-bold text-lg">
