2e3caa98b84dc90e00c44f3dacb682c185e3d3e8
b-sharman <nonoesimposible72@gmail.com>
2024-02-10 05:45:11 -0700

    squashed from camera
    Squashed commit of the following:
    
    commit e886680357e1cdd3dc9dddc93c3191e7009b3411
    Author: b-sharman <nonoesimposible72@gmail.com>
    Date:   Sat Feb 10 05:22:17 2024 -0700
    
        forgot to convert a button to Flowbite
    
    commit 4328b5e3c2cc75469d282f9dd0baec188e2776e3
    Author: b-sharman <nonoesimposible72@gmail.com>
    Date:   Sat Feb 10 05:19:48 2024 -0700
    
        add some s t y l e
    
    commit 2691c95836c9715274da05cde99086d29aee380a
    Author: b-sharman <nonoesimposible72@gmail.com>
    Date:   Sat Feb 10 04:17:47 2024 -0700
    
        use Flowbite modal instead of custom modal
    
    commit 8c11cbacf9e50985379cfec87de48dd9e19651ea
    Author: b-sharman <nonoesimposible72@gmail.com>
    Date:   Sat Feb 10 03:28:58 2024 -0700
    
        remove debug <p>s
    
    commit ca43cdf744fdf43d796652eb3ce5fcf29eeca583
    Author: b-sharman <nonoesimposible72@gmail.com>
    Date:   Sat Feb 10 03:19:51 2024 -0700
    
        move the camera UI to the center thingy
    
    commit bd8d28968e14a71d5c79f1b4dc5b7a4b3397978c
    Author: b-sharman <nonoesimposible72@gmail.com>
    Date:   Sat Feb 10 03:08:24 2024 -0700
    
        stop using the camera when not taking the photo
    
    commit d3656cbdfaa3de920a6a3ecc1006a776aee91620
    Author: b-sharman <nonoesimposible72@gmail.com>
    Date:   Sat Feb 10 02:15:51 2024 -0700
    
        functional camera setup
    
    commit 6eb4ee88130ade5b75e3cc6583716e4739984b9e
    Author: b-sharman <nonoesimposible72@gmail.com>
    Date:   Fri Feb 9 23:30:08 2024 -0700
    
        fluff commit
    
    commit e330f460f34fcdac20cac8c9560da633a0cd1b8e
    Author: b-sharman <nonoesimposible72@gmail.com>
    Date:   Fri Feb 9 23:29:01 2024 -0700
    
        wrap photo taker in a modal
    
    commit bc66456aa6ab7dd8a055f1eb99da9df29d070ae0
    Author: b-sharman <nonoesimposible72@gmail.com>
    Date:   Fri Feb 9 22:51:22 2024 -0700
    
        don't show preview until button is pressed
    
    commit 712033ffef0f0424927276160a066c2bb6860bff
    Author: b-sharman <nonoesimposible72@gmail.com>
    Date:   Fri Feb 9 22:18:42 2024 -0700
    
        add basic webcam preview

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,18 @@
-<script lang="ts">
+<script lang='ts'>
+  import CameraModal from './cameraModal.svelte';
+  import { Button } from 'flowbite-svelte';
+
+  let showModal = false;
+
+  async function openModal() {
+    showModal = true;
+  }
+
 </script>
 
 <div class="w-full h-full flex flex-col justify-center items-center">
   <h1 class="text-2xl font-bold">StandUp</h1>
   <h2 class="text-xl">Stay fit.</h2>
+  <Button on:click={openModal}>Take photo</Button>
+  <CameraModal bind:showModal />
 </div>
