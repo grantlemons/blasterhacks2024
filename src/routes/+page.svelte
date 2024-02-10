b675c5a8b65750155296b39133aeb868c052536a
Elijah Potter <elijah.sirius@protonmail.com>
2024-02-10 09:16:50 -0700

    feat: feed is now own component + on front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	import CameraModal from './cameraModal.svelte';
+	import Feed from '$lib/Feed.svelte';
+	import { SignedIn } from 'sveltefire';
+import CameraModal from './cameraModal.svelte';
 	import { Button } from 'flowbite-svelte';
 
 	let showModal = false;
@@ -9,7 +11,7 @@
 	}
 </script>
 
-<div class="w-full h-full flex flex-col space-y-6 justify-center items-center">
+<div class="w-full h-1/3 flex flex-col space-y-6 justify-center items-center">
 	<div class="flex flex-col justify-center items-center">
 		<h1 class="text-2xl font-bold">StandUp</h1>
 		<h2 class="text-xl">Stay fit.</h2>
@@ -17,3 +19,7 @@
 	<Button class="w-80" on:click={openModal}>Take Photo</Button>
 	<CameraModal bind:showModal />
 </div>
+
+<SignedIn>
+  <Feed/>
+</SignedIn>
--- src/routes/cameraModal.svelte
+++ src/routes/cameraModal.svelte
@@ -1,5 +1,4 @@
 <script lang='ts'>
-  import { onMount } from 'svelte';
   import { Button, Modal } from 'flowbite-svelte';
   import { uploadImage } from '../lib/uploadImage.ts';
 
