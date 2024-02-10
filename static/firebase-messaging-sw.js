731160d80fdcc879c889412813f12eaaca94b28b
Grant Lemons <grant0lemons@gmail.com>
2024-02-10 06:45:33 -0700

    remove redundant notif

--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -7,13 +7,6 @@ importScripts('/__/firebase/init.js');
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
+messaging.onBackgroundMessage(function (payload) {
 	console.log('[firebase-messaging-sw.js] Received background message', payload);
-	// Customize notification here
-	const notificationTitle = 'Background Message Title';
-	const notificationOptions = {
-		body: 'Background Message body.'
-	};
-
-	self.registration.showNotification(notificationTitle, notificationOptions);
 });
