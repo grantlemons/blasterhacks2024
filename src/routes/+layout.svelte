fb94bf889148a84e69c257b27e889121d13b6dbc
Elijah Potter <elijah.sirius@protonmail.com>
2024-02-09 21:14:57 -0700

    Fixed firebase build issues

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
 	import { FirebaseApp } from 'sveltefire';
 	import { initializeApp } from 'firebase/app';
-	import { getFirestore } from 'firebase/firestore';
 	import { getAuth } from 'firebase/auth';
 
-	// Initialize Firebase
-	const app = initializeApp(/* your firebase config */);
-	const firestore = getFirestore(app);
-	const auth = getAuth(app);
+  const firebaseConfig = {
+    apiKey: "AIzaSyCjUnR1Eydjr3qxouIl4fxiXyQvG2auUj8",
+    authDomain: "neovim-btw.firebaseapp.com",
+    projectId: "neovim-btw",
+    storageBucket: "neovim-btw.appspot.com",
+    messagingSenderId: "930780589668",
+    appId: "1:930780589668:web:6472e046a1bddfa5b0f971"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
 </script>
 
-<FirebaseApp {auth} {firestore}>
+<FirebaseApp {auth} {app}>
 	<slot />
 </FirebaseApp>
