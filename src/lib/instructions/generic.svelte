2a8552545cf3e3b6b9d02daef3e42dbd2f558d3d
b-sharman <byron.n.sharman@gmail.com>
2024-02-10 18:01:55 -0700

    don't show photo upload UI before losers log in

--- src/lib/instructions/generic.svelte
+++ src/lib/instructions/generic.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { SignedIn, SignedOut  } from 'sveltefire';
   import { Button } from 'flowbite-svelte';
   import CameraModal from '../../routes/cameraModal.svelte';
 
@@ -9,7 +10,12 @@
 </script>
 
 <main class="flex flex-col justify-between h-full md:justify-normal md:h-min md:w-3/5 p-6">
-  <slot name="instructions-content" />
-  <Button class="mt-4" on:click={() => showModal=true}>Upload a post</Button>
-  <CameraModal bind:showModal />
+  <SignedIn>
+    <slot name="instructions-content" />
+    <Button class="mt-4" on:click={() => showModal=true}>Upload a post</Button>
+    <CameraModal bind:showModal />
+  </SignedIn>
+  <SignedOut>
+    <p>You must log in to participate in the workout.</p>
+  </SignedOut>
 </main>
