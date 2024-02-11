d5baa8278ab3b89b68bddd0027502cbd47a26697
Lukas Werner <lukas.m.werner@outlook.com>
2024-02-10 17:31:22 -0700

    save interval sending code

---
+++ interval/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	firebase "firebase.google.com/go/v4"
+	"firebase.google.com/go/v4/messaging"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	opt := option.WithCredentialsFile("neovim-btw-1b0034d44d78.json")
+	config := &firebase.Config{ProjectID: "neovim-btw"}
+	app, err := firebase.NewApp(context.Background(), config, opt)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	msgr, err := app.Messaging(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	something, err := msgr.Send(context.Background(), &messaging.Message{
+		Notification: &messaging.Notification{
+			Title: "Lets Fuckign GO!",
+			Body:  "Time for a workout!",
+		},
+		Condition: "app.id == '1:930780589668:web:6472e046a1bddfa5b0f971'",
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(something)
+}
