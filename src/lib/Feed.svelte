933ceef6347e8dbfe652fa49a88cff3b61d5bcd1
Elijah Potter <elijah.sirius@protonmail.com>
2024-02-10 18:12:13 -0700

    Fixed lukas' issue (but for real this time)

--- src/lib/Feed.svelte
+++ src/lib/Feed.svelte
@@ -3,17 +3,20 @@
 	import { workoutCollection } from '$lib/workoutPost';
 	import { query, where } from 'firebase/firestore';
 	import { Collection, Doc, SignedIn } from 'sveltefire';
+	import { getFriends } from './friends';
 </script>
 
 <SignedIn let:user>
   <div class="w-full flex flex-col items-center gap-4">
     <Doc ref={`friends/${user.uid}`} let:data>
-      {#if data.friends.length != 0 && data != null}
+        <p slot="loading" class="text-center"><em>You don't have any StandUp friends yet. <br/><a href="/searchuser">Go find some!</a></em></p>
+
+      {#if data == null || data.friends.length == 0}
+        <p class="text-center"><em>You don't have any StandUp friends yet. <br/><a href="/searchuser">Go find some!</a></em></p>
+      {:else}
   	    <Collection ref={query(workoutCollection(), where("userId", 'in', data.friends))} let:data>
 			    <SortedFeed {data} />
   	    </Collection>
-      {:else}
-        <p class="text-center"><em>You don't have any StandUp friends yet. <br/><a href="/searchuser">Go find some!</a></em></p>
       {/if}
     </Doc>
   </div>
